653648aebada9daeb6941e5fdcf696a9886c4518
Erik Svensson <erik.svensson@regionorebrolan.se>
2025-04-05 00:12:25 +0200

    code cleanup

--- src/app/version-content/version-content.component.ts
+++ src/app/version-content/version-content.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { HttpErrorResponse } from '@angular/common/http';
+import { Component, DestroyRef, inject, OnInit } from '@angular/core';
 import { Config } from '../config/config';
 import { ConfigService } from '../config/config.service';
 import { Svc } from './models/svc';
@@ -18,38 +19,51 @@ export class VersionContentComponent implements OnInit {
   versions: VersionWrapper | undefined;
   svcs: Svc[] = [];
   constructor(private versionSvc: VersionService) { }
+  destroyRef = inject(DestroyRef);
 
   ngOnInit(): void {
     ConfigService.Config?.map((cfg) => {
-      this.versionSvc.getVersion(cfg.adminUrl).subscribe({
-        next: (data: VersionWrapper) => {
-          if (data?.data?.version) {
-            this.populateSvcItem(data.data.version, cfg);
-          } else if (data?.data) {
-            this.populateSvcItem(data.data, cfg)
+      new Promise<any>((resolve, reject) => {
+        this.versionSvc.getVersion(cfg.adminUrl).subscribe({
+          next: (data: VersionWrapper) => {
+            if (data?.data) {
+              resolve(data.data);
+            }
+          },
+          error: (err: HttpErrorResponse) => {
+            console.log(err.message);
+            reject();
           }
-        }
+        });
+      }).then((items) => {
+        this.populateSvcItem(items, cfg);
       });
-      this.versionSvc.getVersion(cfg.exposedUrl).subscribe({
-        next: (data: VersionWrapper) => {
-          if (data?.data?.version) {
-            this.populateSvcItem(data.data.version, cfg);
-          } else if (data?.data?.items) {
-            this.populateSvcItem(data.data.items, cfg)
+      new Promise<any>((resolve, reject) => {
+        this.versionSvc.getVersion(cfg.exposedUrl).subscribe({
+          next: (data: VersionWrapper) => {
+            if (data?.data?.version) {
+              resolve(data.data.version);
+            } 
+          },
+          error: (err: HttpErrorResponse) => {
+            console.log(err.message);
+            reject();
           }
-        }
-      })
+        });
+      }).then((items) => {
+        this.populateSvcItem(items, cfg);
+      });
     });
   }
   populateSvcItem(obj: any, cfg: Config) {
     const items = new Map<string, string>();
     for (const key in obj) {
-      if (obj.hasOwnProperty(key) ) {
+      if (obj.hasOwnProperty(key)) {
         items.set(key, obj[key]);
       }
     }
 
-    items.forEach((value: string, key: string)=> {
+    items.forEach((value: string, key: string) => {
       let v = this.svcs.find(n => n.name === key)
       if (!v) {
         v = {
@@ -70,6 +84,6 @@ export class VersionContentComponent implements OnInit {
       })
     })
   }
-  
+
 }
 
