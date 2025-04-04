35aa97a1ab384b8b35ec5025ad64a8349ae19bd4
Erik Svensson <erik.svensson@regionorebrolan.se>
2025-04-04 23:59:02 +0200

    remove commented code and fix newline

--- src/app/version-content/models/version.ts
+++ src/app/version-content/models/version.ts
@@ -7,17 +7,10 @@ export interface VersionWrapper {
 
 export interface Data {
     items?: Map<string, string>;
-
     version?: Map<string, string>;
 }
 
 export interface Version {
     items: Map<string, string>;
-    // "address-api": string
-    // "bankid-api": string
-    // "planet-api": string
-    // "planit": string
-    // "ratten-api": string
-    // "db": string
 }
 
