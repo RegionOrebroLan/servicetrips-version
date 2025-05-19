0c63da583a5a7b7d0e7870138839437f1ab8d4b7
Erik Svensson <erik.svensson@regionorebrolan.se>
2025-05-19 11:53:49 +0200

    add title model

--- src/app/config/config.service.ts
+++ src/app/config/config.service.ts
@@ -1,20 +1,37 @@
 import { HttpClient, HttpErrorResponse } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Config } from './config';
+import { Title } from './title';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ConfigService {
   private static _config: Config[];
+  private static _title: Title;
   constructor(private http: HttpClient) {
   }
 
   public static get Config(): Config[] {
     return ConfigService._config;
   }
+  public static get Title(): Title {
+    return ConfigService._title;
+  }
 
   loadConfig(): Promise<Config[]> {
+    const titleSub = this.http.get<Title>("/config/title.json").subscribe({
+      next: (title: Title) => {
+        ConfigService._title = title;
+      },
+      error: (err: HttpErrorResponse) => {
+        console.log(err.message);
+        titleSub.unsubscribe();
+      },
+      complete: () => {
+        titleSub.unsubscribe();
+      }
+    })
     return new Promise<Config[]>((resolve, reject) => {
       const sub = this.http.get<Config[]>("/config/cfg.json").subscribe({
         next: (config: Config[]) => {
